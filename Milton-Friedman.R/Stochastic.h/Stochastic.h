d0ca99a2064067bde129a5b22c8ed3504dce7900
RubenGavidia0x <rgavidia13@gmail.com>
2021-06-03 18:39:15 -0300

    Update Stochastic.h

--- Milton-Friedman.R/Stochastic.h/Stochastic.h
+++ Milton-Friedman.R/Stochastic.h/Stochastic.h
@@ -41,3 +41,19 @@ Vector aleatorio
    de correlación.
   Distribución de la función de dos variables aleatorias
 
+Estocastica 2
+    Función Generatriz de momentos (FGM)
+      Definición y propiedades
+      La media y la varianza de las distribuciones clásicas utilizando FGM
+    El Teorema del Límite Central (TLC) y su aplicación al muestreo
+      
+    Distribuciones en el muestreo: La Chi—cuadrado, la “t” de Student y la F
+    
+    Decisiones estadísticas. Estimación puntual
+    Estimación por intervalos
+    Prueba de hipótesis estadísticas
+
+Canavos, G. G, Probabilidad y Estadística: Aplicaciones y métodos, McGraw Hill
+Freund, Miller y Johnson, “ Probabilidad y Estadística para Ingenieros” Prentice Hall
+Hoel, Port, Stone, Introduction to Mathematical Statistics, Houghton Mifflin
+
