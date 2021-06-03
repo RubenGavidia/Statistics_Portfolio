5547ee5187517aeccf888c8a077cc78244fb2cad
RubenGavidia0x <rgavidia13@gmail.com>
2021-06-03 16:30:26 -0300

    Create Stochastic.h

---
+++ Milton-Friedman.R/Stochastic.h/Stochastic.h
@@ -0,0 +1,43 @@
+Estocastica 1 o Estadistica Matematica con Aplicaciones
+
+Estadística descriptiva: manejo de datos empíricos
+  Histogramas y polígonos de frecuencia
+  Medidas de la tendencia central y de la dispersión
+  Introducción al muestreo: Muestreo simple, sistemático, por
+   conglomerados, por estratos.  
+  
+Nociones de probabilidad
+  Definiciones básicas
+  Combinatoria
+  Probabilidad condicional e independencia
+  
+La noción de variable aleatoria y conceptos relacionados
+  Los conceptos de variable aleatoria y de distribución de
+   probabilidad
+  Distribución de funciones de variables aleatorias
+  Valores esperados y momentos
+
+Distribuciones clásicas discretas y continuas
+  Distribuciones discretas: La distribución Binomial y la
+   distribución de Poisson. Otras distribuciones 
+   discretas: la Geométrica, la Pascal, la 
+   Hipergeométrica y la Multinomial.
+  Distribuciones continuas: La distribución Uniforme. La
+   distribución Normal. La distribución Exponencial.
+     
+ El Teorema del Límite Central y otros resultados límites
+  La desigualdad de Tchebychev
+  La ley de los números grandes
+  El Teorema del Límite central
+  Aplicaciones del TLC a la distribución Binomial (DeMoivre-
+   Laplace).
+
+Vector aleatorio
+  Los conceptos de Variable aleatoria bi-dimensional y
+   distribución bi-dimensional
+  Distribuciones marginales y condicionales. Variables
+   aleatorias
+  Momentos bi-dimensionales. La co-varianza y el coeficiente
+   de correlación.
+  Distribución de la función de dos variables aleatorias
+
