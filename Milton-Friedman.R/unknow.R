d9366e2dc67e44b21523861c94efee8c7782bc70
RubenGavidia0x <rgavidia13@gmail.com>
2021-06-05 01:41:41 -0300

    Update unknow.R

--- Milton-Friedman.R/unknow.R
+++ Milton-Friedman.R/unknow.R
@@ -1,3 +1,11 @@
 Linear/Multivariate Regression
 OLS Estimators
 Causal Effects
+
+Statistical Methods
+  Decision theory
+  Hypothesis testing
+  Bayesian inference
+  Comparative inference
+  Generalised linear model
+  Ruin theory 
